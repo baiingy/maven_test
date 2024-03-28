6966210e7a38519c34ce0e5045619f1303ed2938
bmyer <252540841@qq.com>
2024-03-28 17:54:20 +0800

    second commit

--- micro-shop/micro-user/src/main/java/com/bmy/Main.java
+++ micro-shop/micro-user/src/main/java/com/bmy/Main.java
@@ -9,6 +9,6 @@ package com.bmy;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello user!");
     }
 }
